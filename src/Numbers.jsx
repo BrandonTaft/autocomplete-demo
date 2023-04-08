958d6efcd99e393828284fbc2e307c755bd7d276
Brandon Taft <btaftcan@gmail.com>
2023-04-07 22:43:39 -0400

    demo eod

---
+++ src/Numbers.jsx
@@ -0,0 +1,36 @@
+import { useState, useRef, useEffect, useMemo } from 'react';
+import { AutoComplete } from 'react-autocomplete-input-component';
+import './App.css';
+
+function Numbers() {
+  const [isOpen, setIsOpen] = useState();
+  const numbers = useRef([]);
+  const toggleDropDown = () => {
+    setIsOpen(isOpen ? false : true)
+  }
+  let x = 30
+  const numberArray = useMemo(() => getArray(x), [x])
+
+
+  return (
+      <section className='content-container' >
+        <AutoComplete
+          list={numberArray}
+          showAll={true}
+          updateIsOpen={(updatedState) => {
+            setIsOpen(updatedState)
+          }}
+          isOpen={isOpen}
+        />
+      </section>
+  );
+
+  function getArray(x) {
+    console.log([...Array(10).keys()])
+    return (
+      [...Array(x).keys()]
+    )
+  }
+}
+
+export default Numbers;
---
+++ src/Preview.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { AutoComplete } from 'react-autocomplete-input-component';
+import axios from 'axios';
+import './App.css';
+
+function Preview() {
+    const [isOpen, setIsOpen] = useState();
+    const [user, setUser] = useState();
+    const [response, setResponse] = useState();
+
+    useEffect(() => {
+        const requestAPI = async () => {
+            try {
+                const res = await axios.get("https://dummyapi.io/data/v1/user?limit=100", {
+                    headers: {
+                        'app-id': '6430b84d15c85b6800a8f933'
+                    },
+                    params: {}
+                });
+                setResponse(res.data.data)
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        requestAPI()
+    }, [])
+
+    return (
+        <section className='preview'>
+             <div className="preview-container">
+                {isOpen && user ?
+                    <>
+                        <span className='green'>Name:</span>
+                        <span>{user.firstName}</span>
+                        <img src={user.picture} alt="profile-pic" width={100} height={100} />
+                    </>
+                    : ""}
+            </div>
+            <AutoComplete
+                list={response}
+                getPropValue={(listName) => listName.firstName}
+                showAll={true}
+                updateIsOpen={(updatedState) => {
+                    setIsOpen(updatedState)
+                }}
+                isOpen={isOpen}
+                handleHighlightedItem={(highlightedItem, list) => {
+                    for (let i = 0; i < list.length; i++) {
+                        if (highlightedItem.innerText === list[i].firstName) {
+                            setUser(list[i])
+                        }
+                    }
+                }}
+            />
+           
+
+        </section>
+    );
+}
+
+export default Preview;
